cfe0b1ae77fab8f4253e2160031026e64f737453
4j534j <4j534j@gmail.com>
2024-06-09 20:53:38 +0300

    valmis

---
+++ index.ts
@@ -0,0 +1,40 @@
+import express, { Express, Request, Response } from "express";
+import cors from 'cors';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
+mongoose.connect(dotenv.config().parsed?.MONGO_URI as string);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+  console.log(error)
+})
+
+database.once('connected', () => {
+  console.log('Database Connected');
+})
+
+const app: Express = express();
+
+app.use(cors({
+    origin: ['http://localhost:3000']
+  }));
+app.use(bodyParser.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+import articleController from './controllers/articleController';
+app.use('/article', articleController);
+
+
+import commentController from "./controllers/commentController";
+app.use('/comment', commentController);
+
+
+
+app.listen(3006,() => {
+  console.log(`[server]: Server is running at http://localhost:3006`);
+});
